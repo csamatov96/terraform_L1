747af18d6ef893790066fe56a333dd221001084b
csamatov96 <996bkg@gmail.com>
2019-08-08 20:25:27 -0500

    cidr_blocks added

--- sec_group.tf
+++ sec_group.tf
@@ -3,17 +3,19 @@ resource "aws_security_group" "my_custom_sec_group" { #
   description = "Allow SSH traffic" #
   vpc_id      = "vpc-e0d5318b" #
 
-  ingress { #
+  ingress { #IN
     # TLS (change to whatever ports you need)
     from_port   = 22 #
     to_port     = 22 #
     protocol    = "tcp" #
     cidr_blocks = ["0.0.0.0/0"] #not secure 
   }
-  egress {
+  egress { #OUT
     from_port       = 0
     to_port         = 0
-    protocol        = "-1"
+    protocol        = "-1" #means all protocl 
+    cidr_blocks = ["0.0.0.0/0"] 
+
   }
 
 }
