86ae32ac73338b12ea7c5797bfa6950dfe94f9cc
csamatov96 <996bkg@gmail.com>
2019-08-08 20:27:24 -0500

    added key name

--- instance.tf
+++ instance.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "test_ec2" {
     ami = "ami-02f706d959cedf892" #hardcoded
     instance_type = "t2.micro" #hardcoded
-    count = "2"
+    #count = "2"
+    key_name   = "Terraform_machine2"
 }
