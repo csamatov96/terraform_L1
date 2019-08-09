35f0634a54d3e37cc6793f4827daa4d611c0aef9
csamatov96 <996bkg@gmail.com>
2019-08-08 20:14:56 -0500

    sec group added SSH

---
+++ sec_group.tf
@@ -0,0 +1,14 @@
+resource "aws_security_group" "my_custom_sec_group" { #
+  name        = "my_custom_sec_group" #
+  description = "Allow SSH traffic" #
+  vpc_id      = "vpc-e0d5318b" #
+
+  ingress { #
+    # TLS (change to whatever ports you need)
+    from_port   = 22 #
+    to_port     = 22 #
+    protocol    = "-1" #
+    cidr_blocks = "0.0.0.0/0" #not secure 
+  }
+
+}
