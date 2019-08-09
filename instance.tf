409b01dc74a7a9f7671aa3eb0264ab0f95b776df
csamatov96 <996bkg@gmail.com>
2019-08-08 20:47:51 -0500

    ...

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "test_ec2" {
     ami = "ami-02f706d959cedf892" #hardcoded
     instance_type = "t2.micro" #hardcoded
-    key_name = "${aws_key_pair.Terraform_machine.key_name}" #full path
+    key_name = "${aws_key_pair.Terraform_machine.key_name}" #full path 
     security_groups = ["${aws_security_group.my_custom_sec_group.name}"] #full path 
     #count = "2"
 
